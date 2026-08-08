71df3789f9b8948fac6b8bdd800c345648681605
saikirankurapti-glitch <saikirankurapti@gmail.com>
2026-08-08 14:45:58 +0530

    Add new CORS origin for Azure static app

--- backend/app/main.py
+++ backend/app/main.py
@@ -123,6 +123,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://salmon-bay-0dd329d10.7.azurestaticapps.net",
     ],
     allow_credentials=True,
     allow_methods=["*"],
